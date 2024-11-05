0155bb2d22237082babbb2c18b02ed310d8a77b5
Jonatan E. Vargas Ortiz <jonvargasor@gmail.com>
2024-11-05 09:26:18 +0100

    Added nvim stable custom

---
+++ init.lua
@@ -0,0 +1,8 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.opt.timeoutlen = 1000
+vim.opt.ttimeoutlen = 0
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
